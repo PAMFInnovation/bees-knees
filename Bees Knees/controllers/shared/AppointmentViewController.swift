208c980c80d1efb3803095f2ac31631c9157d41a
Ben Dapkiewicz <bendapkiewicz@gmail.com>
2016-11-03 08:52:25 -0700

    - started work on appointments view

--- Bees Knees/controllers/shared/AppointmentViewController.swift
+++ Bees Knees/controllers/shared/AppointmentViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 
-class AppointmentViewController: UIViewController {
+class AppointmentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Subviews
+    var scrollView: UIScrollView!
+    var tableView: UITableView!
+    
+    // Table data
+    //
+    
+    // Keep track of the observed UI item in case we need to make it visible via scrolling
+    var activeElement: UIControl?
+    
+    // Keep default edge insets for when we need to reset scrolling
+    var defaultScrollInsets: UIEdgeInsets?
+    
     
     // MARK: - Initialization
     required init?(coder aDecoder: NSCoder) {
@@ -29,10 +43,6 @@ class AppointmentViewController: UIViewController {
         
         // White background
         self.view.backgroundColor = UIColor.white
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
         // Set the title
         self.title = NSLocalizedString("Appointment", comment: "")
@@ -44,14 +54,50 @@ class AppointmentViewController: UIViewController {
         // Add the done button in the navigation bar and color it
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(AppointmentViewController.done))
         self.navigationItem.rightBarButtonItem?.tintColor = UIColor.red
+        
+        // Add items to the table view data
+        //
+        
+        // Setup the scrollview
+        self.scrollView = UIScrollView(frame: self.view.frame)
+        self.view.addSubview(scrollView)
+        
+        // Setup the table view
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height - 120), style: .grouped)
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        self.tableView.separatorStyle = .singleLine
+        self.tableView.isScrollEnabled = true
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        scrollView.addSubview(self.tableView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Listen to keyboard events so we can reposition scroll items
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(keyboardOnScreen), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        center.addObserver(self, selector: #selector(keyboardOffScreen), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        // Unregister from keyboard events
+        let center = NotificationCenter.default
+        center.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        center.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        // Set default scroll insets
+        defaultScrollInsets = scrollView.contentInset
+        
+        // An interactively-dismissed keyboard will dismiss when the user scrolls
+        scrollView.keyboardDismissMode = .interactive
     }
 
     
@@ -62,5 +108,61 @@ class AppointmentViewController: UIViewController {
     
     func done() {
         
+    }
+    
+    
+    // MARK: - Table View Delegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3//tableViewData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Set text and index on the custom cell
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! UITableViewCell
+        
+        return cell
+    }
+    
+    
+    // MARK: - Keyboard events
+    public func keyboardOnScreen(notification: NSNotification) {
+        return;
+        
+        // Get the keyboard rectangle so we can offset our scroll view by its size
+        let info: NSDictionary = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIKeyboardFrameBeginUserInfoKey) as? NSValue)?.cgRectValue.size
+        
+        // Set the content insets of the scroll view using the keyboard's height
+        var contentInsets:UIEdgeInsets = defaultScrollInsets!
+        contentInsets.bottom += kbSize!.height
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+        //--- If the text area collides with the keyboard, we need to move it in view ---//
+        // Get the bounds of the view that we can see, excluding the space occupied by the keyboard
+        var aRect: CGRect = UIScreen.main.bounds
+        aRect.size.height -= (kbSize?.height)!
+        
+        // Convert the text view's center to the root view's coordinate system
+        // We need to do this because we are checking the absolute position of
+        // the text area against the screen bounds
+        let textAreaCenter: CGPoint = CGPoint(x: (self.activeElement?.frame.midX)!, y: (self.activeElement?.frame.midY)!)
+        let convertedPoint: CGPoint = (self.activeElement?.superview?.convert(textAreaCenter, to: self.view))!
+        
+        // If the visible space does not contain the converted point, we need to scroll it in view
+        if (!aRect.contains(convertedPoint)) {
+            let scrollPoint:CGPoint = CGPoint(x: 0.0, y: self.scrollView.contentOffset.y + convertedPoint.y - kbSize!.height)
+            self.scrollView.setContentOffset(scrollPoint, animated: true)
+        }
+    }
+    
+    public func keyboardOffScreen(notification: NSNotification) {
+        // Reset the scroll view insets
+        scrollView.contentInset = defaultScrollInsets!
+        scrollView.scrollIndicatorInsets = defaultScrollInsets!
     }
 }
---
+++ Bees Knees/shared/DateTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DateTableViewCell.swift
+//  Bees Knees
+//
+//  Created by Ben Dapkiewicz on 11/2/16.
+//  Copyright © 2016 Sutter Health. All rights reserved.
+//
+
+import UIKit
+
+
+class DateTableViewCell: ExpandingTableViewCell {
+    
+    // MARK: - Initialization
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
