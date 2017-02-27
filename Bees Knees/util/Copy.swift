f3f1c0c1e12ff0df1c854a669f239bc6f12d6348
Ben Dapkiewicz <bendapkiewicz@gmail.com>
2017-02-27 11:53:14 -0800

    - binder content updated by Tiffany - appointment copy updated by Tiffany

--- Bees Knees/util/Copy.swift
+++ Bees Knees/util/Copy.swift
@@ -16,15 +16,15 @@ class Copy {
             return "" +
                 "Plan to:\n" +
                 "- Bring your binder and medication list\n" +
-                "- Bring your coach, if possible\n" +
-                "- Spend 3 hours for medical testing and class\n" +
+                "- Bring your coach\n" +
+                "- Spend 2-3 hours for medical testing and class\n" +
                 "- Bring your list of questions, if any\n" +
                 "- Bring filled out questionnaires for: Plans for Discharge, Anesthesia, Sleep Apnea"
             
         case .Orthopedic:
             return "" +
                 "Plan to:\n" +
-                "- Bring your coach, if possible\n" +
+                "- Bring your coach\n" +
                 "- Confirm surgery details with your surgeon\n" +
                 "- Bring your binder and list of questions, if any\n" +
                 "- Bring your medication list"
@@ -33,10 +33,10 @@ class Copy {
             return "" +
                 "Plan to:\n" +
                 "- Start HibiClens shower routine 2-3 days before\n" +
-                "- Drink only water the day before and of surgery\n" +
+                "- Follow fasting instructions provided by your surgeon or anesthesiologist\n" +
                 "- Arrive at the hospital at least 2 hours beforehand\n" +
-                "- Bring your coach, if possible\n" +
-                "- Bring your packed bag\n" +
+                "- Bring your coach\n" +
+                "- Bring your binder and packed bag\n" +
                 "- Be asked many questions for surgery safety\n" +
                 "- Use your spirometer before and after surgery"
             
@@ -50,18 +50,16 @@ class Copy {
         case .FollowUp6Week:
             return "" +
                 "Plan to:\n" +
-                "- Bring your coach, if possible\n" +
-                "- Spend 3 hours for medical testing\n" +
-                "- Bring a list of questions\n" +
-                "- Sign documentation"
+                "- Check you range of motion and strength\n" +
+                "- Bring your list of questions, if any\n" +
+                "- Share any difficulties in increasing activity levels"
             
         case .FollowUp12Week:
             return "" +
                 "Plan to:\n" +
-                "- Bring your coach, if possible\n" +
-                "- Spend 3 hours for medical testing\n" +
-                "- Bring a list of questions\n" +
-                "- Sign documentation"
+                "- Check you range of motion and strength\n" +
+                "- Bring your list of questions, if any\n" +
+                "- Share any difficulties in increasing activity levels"
             
         default:
             return ""
