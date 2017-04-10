0f1cd05dbfcc5b5f8c843f51f64e42fac8517082
Kranthi Vallamreddy <kranthi@Aashayas-Mac-mini.local>
2017-04-10 15:48:12 -0700

    Added LongArcQuadsMills activity.

---
+++ Bees Knees/ck-activities/LongArcQuadsMills.swift
@@ -0,0 +1,45 @@
+//
+//  LongArcQuadsMills.swift
+//  Bees Knees
+//
+//  Created by Kranthi Vallamreddy on 4/10/17.
+//  Copyright © 2017 Sutter Health. All rights reserved.
+//
+
+import CareKit
+
+
+struct LongArcQuadsMills: Activity {
+    let activityType: ActivityType = .LongArcQuadsMills
+    
+    let rationale: String = NSLocalizedString("\u{2022} [NEEDS RATIONALE]", comment: "")
+    let image: (name: String, type: String) = ("ref_longarcquads_mills", "png")
+    let video: (name: String, type: String) = ("", "")
+    
+    func carePlanActivity() -> OCKCarePlanActivity {
+        // Create the weekly schedule
+        let startDate = DateComponents(year: 2016, month: 11, day: 01)
+        let schedule = OCKCareSchedule.weeklySchedule(withStartDate: startDate as DateComponents, occurrencesOnEachDay: [3, 3, 3, 3, 3, 3, 3])
+        
+        // Set localized strings to be used in the activity
+        let title = NSLocalizedString("Long Arc Quads", comment: "")
+        let summary = NSLocalizedString("10 reps", comment: "")
+        let instructions = NSLocalizedString("Straighten operated leg and try to hold it for 10 seconds. Relax. Repeat.", comment: "")
+        
+        // Create the intervention activity
+        let activity = OCKCarePlanActivity.intervention(
+            withIdentifier: activityType.rawValue,
+            groupIdentifier: nil,
+            title: title,
+            text: summary,
+            tintColor: Colors.turquoise.color,
+            instructions: instructions,
+            imageURL: Util.getURLForResource(resource: image.name, type: image.type) as URL,
+            schedule: schedule,
+            userInfo: nil
+        )
+        
+        return activity
+    }
+}
+
